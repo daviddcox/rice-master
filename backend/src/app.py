ac86b3566305e4f7666d348c33a6b2e5041568b9
Jaxson Pahukula <jaxpahu@gmail.com>
2024-11-08 21:00:34 -0500

    docker compose dev profile working

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from message import socketio
 
-app = Flask(__name__)
-socketio.init_app(app)
 
 if __name__ == '__main__':
-    print("Starting socket server")
+    print("Starting server...")
+
+    app = Flask(__name__)
+    socketio.init_app(app)
+
     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True) # TODO figure out how to make this prod ready
--- run-dev.sh
+++ run-dev.sh
@@ -4,4 +4,4 @@ clear
 echo "Starting backend in dev mode..."
 
 # Starts the containers in watch mode, which restarts the server when you edit a file in server/src
-docker compose watch --prune
+docker compose --profile dev up --watch --remove-orphans --build
